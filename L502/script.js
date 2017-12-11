e161019188026b556ed8bcd43666a465df210621
Giorgi Parsaghashvili <kjkjknjj@gmail.com>
2017-12-10 20:59:49 -0400

    lab5

---
+++ L502/script.js
@@ -0,0 +1,5 @@
+var users = document.getElementById('users');
+var empty = document.getElementById('empty-list');
+
+alert("users empty: " + (users.children.length == 0));
+alert("empty-list empty: " + (empty.children.length == 0));
